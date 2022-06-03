d557ebc7b723538707c84997980807b83c0636f9
0xmani <35402359+0xmani@users.noreply.github.com>
2022-06-03 09:14:44 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 
 sudo apt update
 sudo apt-get install libleptonica-dev tesseract-ocr libtesseract-dev libtesseract-dev python3-pil tesseract-ocr-eng tesseract-ocr-script-latn -y
-pip3 install -r requirements.txt
+sudo pip3 install -r requirements.txt
