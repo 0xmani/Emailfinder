9b2da55fafcddb85638273d801e965e8c7cab4cc
0xmani <35402359+0xmani@users.noreply.github.com>
2022-06-06 15:07:53 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt-get install libleptonica-dev tesseract-ocr libtesseract-dev libtesseract-dev python3-pil tesseract-ocr-eng tesseract-ocr-script-latn -y
+sudo apt update -y
+sudo apt-get install libleptonica-dev tesseract-ocr libtesseract-dev libtesseract-dev python3-pil tesseract-ocr-eng tesseract-ocr-script-latn firefox-geckodriver -y
 sudo pip3 install -r requirements.txt
-wget https://github.com/mozilla/geckodriver/releases/download/v0.31.0/geckodriver-v0.31.0-linux64.tar.gz
-tar -xvf geckodriver-v0.31.0-linux64.tar.gz
+
 
 
 
